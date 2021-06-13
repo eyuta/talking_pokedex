88b0a283e6d1ac7086ebbf6374a4d3a7eefadf36
eyuta <shion612@gmail.com>
2021-06-14 07:46:17 +0900

    #9  Remove Chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,10 @@
 import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
 
 import { AppProps } from "next/app";
 
 // Do this at the root of your application
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default App;
