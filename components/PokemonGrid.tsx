efa9cb5e9a04967970e6e46b14813d372685d83d
eyuta <shion612@gmail.com>
2021-06-14 09:26:42 +0900

    #9 Fix CSS

--- components/PokemonGrid.tsx
+++ components/PokemonGrid.tsx
@@ -7,25 +7,37 @@ import { FixedSizeGrid } from "react-window";
 import { pokemonList } from "@/data/dist/pokemonList";
 import PokemonItem from "@/components/PokemonItem";
 import { getGridSize } from "@/utils/useWindowDimensions";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 
-const pokemonCount = pokemonList.length;
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      paddingTop: "64px",
+      height: "100vh",
+    },
+  })
+);
 
 export default function PokemonGrid() {
+  const classes = useStyles();
   const { columnCount, columnWidth, rowHeight } = getGridSize();
+
   return (
-    <AutoSizer>
-      {({ height, width }) => (
-        <FixedSizeGrid
-          columnCount={columnCount}
-          columnWidth={columnWidth}
-          height={height}
-          rowCount={Math.ceil(pokemonCount / columnCount)}
-          rowHeight={rowHeight}
-          width={width}
-        >
-          {PokemonItem}
-        </FixedSizeGrid>
-      )}
-    </AutoSizer>
+    <div className={classes.root}>
+      <AutoSizer>
+        {({ height, width }) => (
+          <FixedSizeGrid
+            columnCount={columnCount}
+            columnWidth={columnWidth}
+            height={height}
+            rowCount={Math.ceil(pokemonList.length / columnCount)}
+            rowHeight={rowHeight}
+            width={width}
+          >
+            {PokemonItem}
+          </FixedSizeGrid>
+        )}
+      </AutoSizer>
+    </div>
   );
 }
